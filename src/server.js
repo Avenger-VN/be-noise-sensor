af09420c4d5124f7f8120269b4d6876012247125
NguyenMinhChien <minhchien03112003@gmail.com>
2023-10-20 19:22:13 +0700

    #1 init project

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const connectDB = require('./config/dbConnect')
+const app = express();
+
+
+connectDB();
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
